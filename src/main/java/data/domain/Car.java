4c0d0e3fda75fb839db4b1f0c1380852ea35a9ea
seok <seogi89@gmail.com>
2020-12-17 15:18:46 +0900

    feat : 자동차가 이름을 가지도록 변경

--- src/main/java/data/domain/Car.java
+++ src/main/java/data/domain/Car.java
@@ -4,8 +4,16 @@ import static data.domain.RacingGameConstants.THRESHOLD;
 
 public class Car {
 
-
+    private Name name;
     private int position;
+
+    private Car(final Name name) {
+        this.name = name;
+    }
+
+    public static Car of(String name) {
+        return new Car(Name.of(name));
+    }
 
     public int move(int power) {
         if (power >= THRESHOLD) {
--- src/main/java/data/domain/RacingGameConstants.java
+++ src/main/java/data/domain/RacingGameConstants.java
@@ -1,8 +1,8 @@
 package data.domain;
 
-public final class RacingGameConstants {
+public interface RacingGameConstants {
 
-    protected static final int THRESHOLD = 4;
-    protected static final int NAME_MAX_LENGTH = 5;
+    int THRESHOLD = 4;
+    int NAME_MAX_LENGTH = 5;
 
 }
