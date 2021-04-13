5b3504803be0f8b75bb38af60a53c5508dfa439f
seok2 <seogi89@gmail.com>
2021-04-13 23:23:46 +0900

    <test> 코드 수정으로 동작하지 않는 테스트 코드 수정

--- src/test/java/data/CarTest.java
+++ src/test/java/data/CarTest.java
@@ -13,23 +13,23 @@ public class CarTest {
     @Test
     public void move() {
         Car car = new Car(new Name("seok2"), () -> 5);
-        assertThat(car.move()).isEqualTo(1);
+        assertThat(car.move()).isEqualTo(new Record("seok2", 1));
     }
 
     @DisplayName("자동차는 4보다 작은 값을 가질 때 전진 할 수 없습니다.")
     @Test
     public void hold() {
         Car car = new Car(new Name("seok2"), () -> 3);
-        assertThat(car.move()).isEqualTo(0);
+        assertThat(car.move()).isEqualTo(new Record("seok2", 0));
     }
 
     @DisplayName("자동차는 이동 테스트 - 4, 3, 2, 5 일때")
     @Test
     public void move_2() {
         Car car = new Car(new Name("seok2"), new RepeatPowerEngine(List.of(4, 3, 2, 5)));
-        assertThat(car.move()).isEqualTo(1);
-        assertThat(car.move()).isEqualTo(1);
-        assertThat(car.move()).isEqualTo(1);
-        assertThat(car.move()).isEqualTo(2);
+        assertThat(car.move()).isEqualTo(new Record("seok2", 1));
+        assertThat(car.move()).isEqualTo(new Record("seok2", 1));
+        assertThat(car.move()).isEqualTo(new Record("seok2", 1));
+        assertThat(car.move()).isEqualTo(new Record("seok2", 2));
     }
 }
